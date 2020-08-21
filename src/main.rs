678959cea9639b12936ad935d45c3bab8a92fc44
Bobby McShane <mcshane.bobby@gmail.com>
2020-08-21 14:01:26 -0400

    Use the heap more strategically to minimize the memory footprint as we iterate through files
    Use a MaxHeap (pop-order is newest access time first). Populate the heap
    until we have enough files in it to reach our target capacity. From then
    on, as we add files to the heap, pop the most-recently-accessed ones
    that we no longer need to clean up to reach our target.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use walkdir::WalkDir;
 use chrono::prelude::*;
 use std::collections::BinaryHeap;
 use std::path::PathBuf;
-use std::cmp::{Reverse, Ordering};
+use std::cmp::Ordering;
 
 #[derive(PartialEq, Eq)]
 struct FileInfo {
@@ -24,19 +24,40 @@ impl Ord for FileInfo {
 }
 
 fn main() {
-    let mut heap = BinaryHeap::new();
+    let mut files_to_delete = BinaryHeap::<FileInfo>::new();
+    let mut aggregate_heap_file_size = 0;
+    let n_bytes_to_delete = 10 * 1024 * 1024; // 10 mb
 
     for entry in WalkDir::new(".")
         .into_iter()
         .filter_map(|entry| entry.ok()) {
         if let Ok(metadata) = entry.metadata() {
             if metadata.is_file() {
-                heap.push(Reverse(FileInfo { accessed: metadata.accessed().unwrap().into(), size : metadata.len(), path : entry.into_path() }) )
+                let accessed = metadata.accessed().unwrap().into();
+                if aggregate_heap_file_size < n_bytes_to_delete || accessed > files_to_delete.peek().unwrap().accessed  {
+                    // NOTE: if our aggregate heap file size is above capacity, we _must_ have something
+                    // in the heap already
+                    let file = FileInfo { accessed: accessed, size : metadata.len(), path : entry.into_path() };
+                    aggregate_heap_file_size += file.size;
+                    files_to_delete.push(file);
+
+                    // NOTE: we should always have at least one file on the heap at this point
+                    while aggregate_heap_file_size - files_to_delete.peek().unwrap().size > n_bytes_to_delete {
+                        // forget about any newer files that we no longer need to delete now that we have
+                        // pushed an older file onto the heap
+                        aggregate_heap_file_size -= files_to_delete.pop().unwrap().size;
+                    }
+                } else {
+                    // if our file is newer than the newest thing already on the heap, and our heap
+                    // is already at capacity, there's no sense in pushing the file onto the heap
+                    // only to remove it immediately afterward
+                }
             }
         }
     }
 
-    while let Some(Reverse(file)) = heap.pop() {
+    while let Some(file) = files_to_delete.pop() {
         println!("{} {}", file.accessed.format("%d/%m/%Y %T"), file.path.display());
     }
+    println!("Deleting {} bytes", aggregate_heap_file_size);
 }
