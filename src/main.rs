41c9b1657b6f932f996460caef9294ee26a58a2d
Bobby McShane <mcshane.bobby@gmail.com>
2020-08-23 11:34:28 -0400

    Add argument parsing as well as a filter which prevents users from deleting files that are newer than X minutes old

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use argh::FromArgs;
 use walkdir::WalkDir;
 use chrono::prelude::*;
+use chrono::Duration;
 use std::collections::BinaryHeap;
 use std::path::PathBuf;
 use std::cmp::Ordering;
@@ -25,37 +27,52 @@ impl Ord for FileInfo {
     }
 }
 
+#[derive(FromArgs)]
+/// Turn your filesystem into an LRU cache by running this program periodically. When run, if the
+/// filesystem for the provided path has fewer than --target-available-space free bytes, delete
+/// files in least-recently-accessed order until the target is reached.
 struct Args {
+    #[argh(switch)]
     /// if provided, do not remove any files and instead print file paths which would be removed if
     /// the program were to be run with the given arguments
     dry_run : bool,
 
+    #[argh(option, short = 't')]
+    /// the minimum empty filesystem space in bytes to leave available for use
+    target_available_space : u64,
+
+    #[argh(option, short = 'o', default = "0")]
+    /// only delete files that were last accessed more than --older-than minutes ago
+    older_than : i64,
+
+    #[argh(positional)]
+    /// the top-level directory at which to recursively reclaim files when the filesystem capacity
+    /// exceeds the target
+    path : PathBuf,
+
+    #[argh(switch, short = 'v')]
     /// enable verbose logging
     verbose : bool,
-
-    /// the minimum empty filesystem space in bytes to leave available for use
-    target_available_capacity : u64,
 }
 
 fn main() {
-    let current_available_space = fs2::available_space(".").unwrap();
-    // TODO: argument parsing
-    let args = Args { dry_run : true, verbose : false, target_available_capacity : current_available_space + 10 * 1024 * 1024 * 1024 };
-    // TODO: end argument parsing
+    let args: Args = argh::from_env();
+    let current_available_space = fs2::available_space(&args.path).unwrap();
+    let older_than_time = Local::now() - Duration::minutes(args.older_than);
 
     let mut n_bytes_deleted = 0;
-    if current_available_space < args.target_available_capacity {
+    if current_available_space < args.target_available_space {
         let mut files_to_delete = BinaryHeap::<FileInfo>::new();
         let mut aggregate_heap_file_size = 0;
-        let max_n_bytes_to_delete = args.target_available_capacity - current_available_space;
+        let max_n_bytes_to_delete = args.target_available_space - current_available_space;
 
-        for entry in WalkDir::new(".")
+        for entry in WalkDir::new(&args.path)
             .into_iter()
             .filter_map(|entry| entry.ok()) {
             if let Ok(metadata) = entry.metadata() {
                 if metadata.is_file() {
                     let accessed = metadata.accessed().unwrap().into();
-                    if aggregate_heap_file_size < max_n_bytes_to_delete || accessed <= files_to_delete.peek().unwrap().accessed  {
+                    if accessed < older_than_time && (aggregate_heap_file_size < max_n_bytes_to_delete || accessed <= files_to_delete.peek().unwrap().accessed) {
                         // NOTE: if our aggregate heap file size is above capacity, we _must_ have something
                         // in the heap already
                         let file = FileInfo { accessed: accessed, size : metadata.len(), path : entry.into_path() };
@@ -82,7 +99,7 @@ fn main() {
         // less-recently used stuff
 
         // re-query available space in case our capacity has been reduced since we started running the program
-        let n_bytes_to_delete = args.target_available_capacity as i64 - fs2::available_space(".").unwrap() as i64;
+        let n_bytes_to_delete = args.target_available_space as i64 - fs2::available_space(&args.path).unwrap() as i64;
         if n_bytes_to_delete > 0 {
             while let Some(file) = files_to_delete.peek() {
                 // if the space we need to reclaim has shrunk since we initially queried it (prior
@@ -96,15 +113,18 @@ fn main() {
                 }
             }
             while let Some(file) = files_to_delete.pop() {
-                n_bytes_deleted += file.size;
                 if args.dry_run {
+                    n_bytes_deleted += file.size;
                     println!("{} {}", file.accessed.format("%m/%d/%Y %T"), file.path.display());
                 } else if remove_file(&file.path).is_ok() && args.verbose {
+                    n_bytes_deleted += file.size;
                     println!("Deleted {} {}", file.accessed.format("%m/%d/%Y %T"), file.path.display());
                 }
             }
         }
     }
 
-    println!("Deleted {} bytes", n_bytes_deleted);
+    if args.verbose {
+        println!("Deleted {} bytes", n_bytes_deleted);
+    }
 }
